63d0c4311681889e3780551f96fec58a0dad27aa
Wellington Pereira Marques <114716548+WMarques25@users.noreply.github.com>
2025-03-31 16:15:26 -0300

    3.1

--- exercicios/cap3.hs
+++ exercicios/cap3.hs
@@ -1,1 +1,26 @@
 -- CAP3 
+-- 3.1 
+data Pergunta = Sim | Nao deriving Show
+
+pergNum :: Pergunta -> Int
+pergNum Sim = 1
+pergNum Nao = 0
+
+listPergs :: [Pergunta] -> [Int]
+listPergs [] = []
+listPergs (x:xs) = pergNum x : listPergs xs
+
+pergToBool :: Pergunta -> Bool
+pergToBool Nao = False
+pergToBool Sim = True
+
+and' :: Pergunta -> Pergunta -> [Bool]
+and' x y = [pergToBool x, pergToBool y, pergToBool x && pergToBool y]
+
+or' :: Pergunta -> Pergunta -> [Bool]
+or' x y = [pergToBool x, pergToBool y, pergToBool x || pergToBool y]
+
+not' :: Pergunta -> Bool
+not' x = not (pergToBool x)
+
+-- 3.2
